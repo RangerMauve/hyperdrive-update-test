67344717bb0575cd7a5e7299b65066956d82c0ce
mauve <ranger@mauve.moe>
2020-07-10 13:43:59 -0400

    Test case demoing the failure

---
+++ index.js
@@ -0,0 +1,24 @@
+const hyperdrive = require('hyperdrive')
+const RAM = require('random-access-memory')
+
+const writer = hyperdrive(RAM)
+
+const WRITE_DELAY = 1000
+
+writer.ready(() => {
+  const reader = hyperdrive(RAM, writer.key)
+
+  function write () {
+    writer.writeFile('/example.txt', `${new Date()}: Hello World!`, () => console.log('wrote'))
+  }
+
+  reader.on('update', () => console.log('update'))
+  writer.once('peer-open', () => setInterval(write, WRITE_DELAY))
+
+  reader.ready(() => {
+    const stream1 = writer.replicate(true, { live: true })
+    const stream2 = reader.replicate(false, { live: true })
+
+    stream1.pipe(stream2).pipe(stream1)
+  })
+})
